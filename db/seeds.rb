d7e9405aa33bdbb2d154a2ab30585cb952ad6658
Brodie Giesbrecht <brodiejgiesbrecht@gmail.com>
2020-10-09 13:33:51 -0500

    Seed script import planets

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,13 @@
 # https://swapi.dev/documentation#ruby
+require "swapi"
+
+planets = Swapi.get_all("planets")["results"]
+
+planets.each do |planet|
+  new_planet = Planet.create(
+    name:    planet["name"],
+    climate: planet["climate"],
+    terrain: planet["terrain"]
+  )
+  puts "Invalid planet #{planet['name']}!" unless new_planet&.valid?
+end
