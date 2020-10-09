0fe6be1072525ca1e813bf1b6fc6dc5d360bd7fd
Brodie Giesbrecht <brodiejgiesbrecht@gmail.com>
2020-10-09 13:43:39 -0500

    Updated seed to create terrain associations

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,30 @@
 # https://swapi.dev/documentation#ruby
 require "swapi"
 
+PlanetaryTerrain.delete_all
+Terrain.delete_all
+Planets.delete_all
+
 planets = Swapi.get_all("planets")["results"]
 
 planets.each do |planet|
+  # Create the planets
+
   new_planet = Planet.create(
     name:    planet["name"],
-    climate: planet["climate"],
-    terrain: planet["terrain"]
+    climate: planet["climate"]
   )
-  puts "Invalid planet #{planet['name']}!" unless new_planet&.valid?
+
+  unless new_planet&.valid?
+    puts "Invalid planet #{planet['name']}!"
+    next
+  end
+
+  # Create the terrains
+  terrains = planet["terrain"].split(",").map(&:strip)
+
+  terrains.each do |terrain|
+    new_terrain = Terrain.find_or_create_by(name: terrain)
+    PlanetaryTerrain.create(planet: new_planet, terrain: new_terrain)
+  end
 end
