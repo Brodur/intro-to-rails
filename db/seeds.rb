4622cb101858ecc7906c8a7e98399f8dd68cf612
Brodie Giesbrecht <brodiejgiesbrecht@gmail.com>
2020-10-09 14:09:55 -0500

    Added film handling to seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,12 +4,30 @@ require "swapi"
 PlanetaryTerrain.delete_all
 Terrain.delete_all
 Planet.delete_all
+Movie.delete_all
+
+# Create Films
+
+films = Swapi.get_all("films")["results"]
+
+films.each do |film|
+  new_film = Film.find_or_create_by(
+    title:        film["title"],
+    episode:      film["episode_id"],
+    release_date: film["release_date"]
+  )
+
+  unless new_film&.valid?
+    puts "Invalid film #{film['title']}!"
+    next
+  end
+end
+
+# Create the planets
 
 planets = Swapi.get_all("planets")["results"]
 
 planets.each do |planet|
-  # Create the planets
-
   new_planet = Planet.find_or_create_by(
     name:    planet["name"],
     climate: planet["climate"]
@@ -26,22 +44,5 @@ planets.each do |planet|
   terrains.each do |terrain|
     new_terrain = Terrain.find_or_create_by(name: terrain)
     PlanetaryTerrain.create(planet: new_planet, terrain: new_terrain)
-  end
-
-  # Create Films
-
-  films = Swapi.get_all("films")["results"]
-
-  films.each do |film|
-    new_film = Film.find_or_create_by(
-      title:        film["title"],
-      episode:      film["episode_id"],
-      release_date: film["release_date"]
-    )
-
-    unless new_film&.valid?
-      puts "Invalid film #{film['title']}!"
-      next
-    end
   end
 end
