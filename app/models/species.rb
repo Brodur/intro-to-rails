cabe58ec530b1db69aa72cc4f339077c9252fe0d
Brodie Giesbrecht <brodiejgiesbrecht@gmail.com>
2020-10-09 15:18:06 -0500

    Created validations for species

--- app/models/species.rb
+++ app/models/species.rb
@@ -1,3 +1,7 @@
 class Species < ApplicationRecord
   belongs_to :planet
+
+  validates :name, :classification, :designation, :average_lifespan, presence: true
+  validates :name, uniqueness: true
+  validates :average_lifespan, numericality: true
 end
