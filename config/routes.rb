7192388c1e5352239d00a21988fe11127f0286c0
Brodie Giesbrecht <brodiejgiesbrecht@gmail.com>
2020-10-10 10:53:39 -0500

    Created planets controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'planets/index'
+  get 'planets/show'
   get 'species/index'
   get 'species/show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
