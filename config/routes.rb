bee9baa0a3c43a13870b38d75e2aea40ec11348c
Brodie Giesbrecht <brodiejgiesbrecht@gmail.com>
2020-10-11 15:09:16 -0500

    Created Search controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'search/index'
+  get 'search/show'
   resources :planets, only: %i[index show]
   resources :species, only: %i[index show]
   resources :films, only: %i[index show]
