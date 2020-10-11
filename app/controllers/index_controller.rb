c77a7286fdcbfa5c2948c88071737f9f35135331
Brodie Giesbrecht <brodiejgiesbrecht@gmail.com>
2020-10-11 09:20:02 -0500

    Updated index page

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,5 +1,7 @@
 class IndexController < ApplicationController
   def index
-
+    @films = Film.limit(3).order("RANDOM()")
+    @planets = Planet.limit(3).order("RANDOM()")
+    @species = Species.limit(3).order("RANDOM()")
   end
 end
