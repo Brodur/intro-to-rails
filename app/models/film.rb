7b7ecf3d45c71632bf2a35920ed775721219ed43
Brodie Giesbrecht <brodiejgiesbrecht@gmail.com>
2020-10-11 10:16:37 -0500

    Associated species to films

--- app/models/film.rb
+++ app/models/film.rb
@@ -2,6 +2,9 @@ class Film < ApplicationRecord
   has_many :planet_appearances
   has_many :planets, through: :planet_appearances
 
+  has_many :species_appearances
+  has_many :species, through: :species_appearances
+
   validates :title, :episode, :opening_crawl, presence: true
   validates :episode, numericality: true
   validates :title, :episode, uniqueness: true
--- app/models/species.rb
+++ app/models/species.rb
@@ -1,6 +1,9 @@
 class Species < ApplicationRecord
   belongs_to :planet, optional: true
 
+  has_many :species_appearances
+  has_many :films, through: :species_appearances
+
   validates :name, :classification, :designation, :average_lifespan, presence: true
   validates :name, uniqueness: true
 end
